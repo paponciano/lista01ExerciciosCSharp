37b2f9d165a2c84913dfbf59d630f9b7056fe697
Paulo Augusto Ponciano <paponciano@gmail.com>
2021-10-19 12:10:34 -0300

    Commit do exercício 01

---
+++ exercicio01/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace exercicio01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a primeira nota: ");
+            decimal nota01 = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("Digite a segunda nota: ");
+            decimal nota02 = Convert.ToDecimal(Console.ReadLine());
+
+            decimal media = (nota01 + nota02) / 2;
+
+            if (media == 10)
+            {
+                Console.WriteLine("Aprovado com distinção");
+            }
+            else if (media >= 7)
+            {
+                Console.WriteLine("Aprovado");
+            }
+            else
+            {
+                Console.WriteLine("Reprovado");
+            }
+        }
+    }
+}
